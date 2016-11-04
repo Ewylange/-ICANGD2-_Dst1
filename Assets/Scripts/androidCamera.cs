13ad0496edcfc936a0a0f9b0c1124db9bcd2a35f
James Petit <jp.james.petit@gmail.com>
2016-11-04 12:28:18 +0100

    Carré détectant les doigts.

--- Assets/Scripts/androidCamera.cs
+++ Assets/Scripts/androidCamera.cs
@@ -29,54 +29,61 @@ public class androidCamera : MonoBehaviour
 
 	void Update () 
 	{
-//Horizontal et Vertical
-		if (Input.touchCount == 1 ) 
-		{
-			if (Input.GetTouch(0).phase == TouchPhase.Moved)
-			{
-				Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-				//float x = Mathf.Clamp(touchDeltaPosition.x * (fov), leftLimit, rightLimit);
-            	//float y = Mathf.Clamp(touchDeltaPosition.y * (fov), lowLimit, highLimit);
-            	transform.Translate( -touchDeltaPosition.x * (fov)/2 * Time.deltaTime, -touchDeltaPosition.y * (fov)/2 * Time.deltaTime, 0);
-			}
-        }
+////Horizontal et Vertical
+//		if (Input.touchCount == 1 ) 
+//		{
+//			if (Input.GetTouch(0).phase == TouchPhase.Moved)
+//			{
+//				Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+//				float x = Mathf.Clamp(touchDeltaPosition.x * (fov), leftLimit, rightLimit);
+//            	float y = Mathf.Clamp(touchDeltaPosition.y * (fov), lowLimit, highLimit);
+//				Vector3 worldDelta = Camera.main.ScreenToWorldPoint(
+//					new Vector3(
+//						touchDeltaPosition.x,
+//						touchDeltaPosition.y,
+//						0
+//					)
+//				);
+//            	transform.Translate( -touchDeltaPosition.x * (fov)/2 * Time.deltaTime, -touchDeltaPosition.y * (fov)/2 * Time.deltaTime, 0);
+//			}
+//        }
 //Zoom et Dezoom
 
 		if (Input.touchCount == 2)
         {
             // Store both touches.
-            if (!rotating){
-            	Touch touchZero = Input.GetTouch(0);
-	            Touch touchOne = Input.GetTouch(1);
+        	Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
 
-	            // Find the position in the previous frame of each touch.
-	            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-	            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+            // Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - new Vector2(Screen.width, Screen.height) / 2;
+			Vector2 touchOnePrevPos = touchOne.position - new Vector2(Screen.width, Screen.height) / 2;
 
-	            // Find the magnitude of the vector (the distance) between the touches in each frame.
-	            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-	            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-	            // Find the difference in the distances between each frame.
-	            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-	            Vector2 mPos = (touchZeroPrevPos - touchOnePrevPos)/2 + touchOnePrevPos;
-	            Vector3 middlePos =  Camera.main.ScreenToWorldPoint(mPos);
+            Vector2 mPos = (touchZeroPrevPos - touchOnePrevPos)/2 + touchOnePrevPos;
+            Vector3 middlePos =  Camera.main.ScreenToWorldPoint(mPos);
 
-	            if (deltaMagnitudeDiff < 0)
+            if (deltaMagnitudeDiff < 0)
+			{
+				if(fov > minFOV)
 				{
-					if(fov > minFOV){
-						zoomTowards(middlePos, -1);
-					}
+					zoomTowards(middlePos, -1);
 				}
-				else if(deltaMagnitudeDiff > 0)
-				{	
-					if(fov < maxFOV){
-						zoomTowards(middlePos, 1);
-					}
-				}  
 			}
-            }
+			else if(deltaMagnitudeDiff > 0)
+			{	
+				if(fov < maxFOV)
+				{
+					zoomTowards(middlePos, 1);
+				}
+			}  
+		}
 	}
 
 	void zoomTowards(Vector3 pos, float direction)
