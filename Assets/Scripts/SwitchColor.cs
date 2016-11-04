09296ae0394fa97261c0c1ba12791850fa26b4c0
Charlene ROSE <charlenerose1208@gmail.com>
2016-11-04 12:38:16 +0100

    Switch Color

---
+++ Assets/Scripts/SwitchColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchColor : MonoBehaviour {
+
+	public Material colorCube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//this.gameObject.GetComponent<Material>().color = colorCube;
+		this.gameObject.GetComponent<Renderer>().material= colorCube;
+	}
+}
