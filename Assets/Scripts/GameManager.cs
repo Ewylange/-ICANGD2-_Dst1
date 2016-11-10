bc944c53bd558f1c82eafc04bde6363230befcdb
James Petit <jp.james.petit@gmail.com>
2016-11-10 16:43:05 +0100

    Rangement.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	void Awake() 
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		QualitySettings.vSyncCount = 0;  
+		Application.targetFrameRate = 45;
+	}
+}
