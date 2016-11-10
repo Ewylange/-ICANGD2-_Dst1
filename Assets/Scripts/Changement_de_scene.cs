eff124cfa497f081ecdd93b833c589ecfbe8e564
Charlene ROSE <charlenerose1208@gmail.com>
2016-11-10 17:29:59 +0100

    merge

--- Assets/Scripts/Changement_de_scene.cs
+++ Assets/Scripts/Changement_de_scene.cs
@@ -1,36 +1,45 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class Changement_de_scene : MonoBehaviour 
-{
-	void Update () 
-	{
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			SceneManager.LoadScene("Switch_scene2");
+public class Changement_de_scene : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+//
+//		if (Input.GetKey(KeyCode.UpArrow))
+//			{
+//	
 //			Application.LoadLevel("Switch_scene2");
-		}
+//		}
+
 	}
 
-	public void LoadLevel(int level) 
-	{
-		SceneManager.LoadScene("Game");
-//		Application.LoadLevel("Game");
+	public void LoadLevel(int level) {
+		
+		Application.LoadLevel("Game");
+	}
+
+	// Quit the game
+	public void QuitGame() {
+		Application.Quit();
 	}
 
 	// Reload this level
+<<<<<<< f83ec3f08cdb3acea2cf7fe0b2908b1215e9e9fb
 	public void ReloadLevel() 
 	{
 		Scene scene = SceneManager.GetActiveScene();
 		SceneManager.LoadScene(scene.name);
 //		Application.LoadLevel(Application.loadedLevel);
+=======
+	public void ReloadLevel() {
+		Application.LoadLevel(Application.loadedLevel);
+>>>>>>> Correction Bug Changement  de scene
 	}
 
 	// Go back to main menu
-	public void BackToMainMenu() 
-	{
-		SceneManager.LoadScene("Menu");
-//		Application.LoadLevel("Menu");
+	public void BackToMainMenu() {
+		Application.LoadLevel("Menu");
 	}
 }
