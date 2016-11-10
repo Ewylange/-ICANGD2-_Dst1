3c5a75fc3cd9909daf8330d0e9ed2abede327717
Charlene ROSE <charlenerose1208@gmail.com>
2016-11-10 12:51:10 +0100

    Début mise en place Menu

---
+++ Assets/Scripts/Changement_de_scene.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Changement_de_scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.UpArrow))
+			{
+	
+			Application.LoadLevel("Switch_scene2");
+		}
+
+
+
+
+	}
+
+	public void LoadLevel(int level) {
+		
+		Application.LoadLevel("Game");
+	}
+
+	// Quit the game
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+	// Reload this level
+	public void ReloadLevel() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	// Go back to main menu
+	public void BackToMainMenu() {
+		Application.LoadLevel("Menu");
+	}
+}
