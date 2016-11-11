b0dc920858a1683b0bf45846a3d215e3cea1693e
AntoineIturbide <antoine.iturbide@gmail.com>
2016-11-11 20:45:37 +0100

    Debug TouchDrag

--- Assets/Scripts/TouchDrag.cs
+++ Assets/Scripts/TouchDrag.cs
@@ -14,6 +14,8 @@ public class TouchDrag : MonoBehaviour
 
     public Camera dragCamera;
 
+    private Vector3 startDragPosition;
+
     public void Awake() {
         SceneManager.sceneLoaded += OnSceneLoad;
     }
@@ -68,7 +70,7 @@ public class TouchDrag : MonoBehaviour
 		}
     }
 
-    void Update() {
+    void Update2() {
         if (dragCamera == null)
             return;
 
@@ -90,15 +92,57 @@ public class TouchDrag : MonoBehaviour
                 toDrag = hit.transform;
                 dist = hit.transform.localPosition.z - dragCamera.transform.localPosition.z;
                 v3 = new Vector3(pos.x, pos.y, dist);
+                startDragPosition = v3;
                 v3 = dragCamera.ScreenToWorldPoint(v3);
                 offset = toDrag.localPosition - v3;
                 dragging = true;
             }
         }
         if (dragging && touch.phase == TouchPhase.Moved) {
+            v3 = new Vector3(pos.x, pos.y, dist);
             v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
             v3 = dragCamera.ScreenToWorldPoint(v3);
+
+
             toDrag.localPosition = v3 + offset;
+        }
+        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
+            dragging = false;
+            toDrag.gameObject.GetComponent<ColorChannel>().EndDrag();
+        }
+    }
+
+    void Update() {
+        if (dragCamera == null)
+            return;
+        
+        if (Input.touchCount != 1) {
+            dragging = false;
+            return;
+        }
+
+        Touch touch = Input.touches[0];
+        Vector3 touchPos = touch.position;
+
+        if (touch.phase == TouchPhase.Began) {
+            RaycastHit hit;
+            Ray ray = dragCamera.ScreenPointToRay(touchPos);
+            LayerMask mask = LayerMask.GetMask("Flying Cube Debug");
+            if (Physics.Raycast(ray, out hit, maxDistance: 15f, layerMask: mask) && (hit.collider.tag == "Draggable")) {
+                // Audio feedback
+                audioSource.PlayOneShot(select);
+                // Retrive the transform selected for this drag
+                toDrag = hit.transform;
+                // Get the distance from camera
+                dist = hit.transform.localPosition.z;
+                // Set dragging flag
+                dragging = true;
+            }
+        }
+        if (dragging && touch.phase == TouchPhase.Moved) {
+            touchPos.z = dist;
+            Vector3 targetPos = dragCamera.ScreenToWorldPoint(touchPos);            
+            toDrag.position = targetPos;
         }
         if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
             dragging = false;
